8d9f26b117357c3f2a47fb52828aa145c67fd36b
SAHIL SOURAV <136837970+SahilSourav23@users.noreply.github.com>
2023-06-17 11:42:29 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,74 @@
+'''num=int(input("enter any no:-"))
+if(num>=0):
+    print("positive enter")
+else:
+        print("Negative Number")'''
+# Divibility Check
+'''
+num=int(input("enter number"))
+if((num%5==0)and(num%3==0)):
+    print("divisible")
+else:
+    print("not divisible")
+    '''
+# WAP TO INPUT CHARACTER AND CHECK WEATHER IT IS VOWEL OR CONSONANT
+'''
+ALPHABATE=input("Enter any Alphabate")
+if(ALPHABATE=='a' or ALPHABATE=='e' or ALPHABATE=='i' or ALPHABATE=='o' or ALPHABATE=='u'or ALPHABATE=='A' or ALPHABATE=='E' or ALPHABATE=='I' or ALPHABATE=='O' or ALPHABATE=='U'):
+    print("vowel")
+else:
+    print("Consonant")'''
+#Multiple Condition
+#
+'''
+marks=int(input("Enter marks"))
+if(marks>90 and marks<100):
+    print("A+")
+elif(marks>=80 and marks<90):
+    print("A")
+elif(marks>=70 and marks<60):
+    print("B+")
+elif(marks>=60 and marks<50):
+    print("B")
+elif(marks>=50 and marks<40):
+    print("c")
+elif(marks>=40 and marks<30):
+    print("d")
+else:
+    print("f")
+    '''
+# Wap to foind greatest one
+'''n1=int(input("Number1"))
+n2=int(input("Number2"))
+n3=int(input("Number3"))
+if(n1>n2 and n1>n3):
+       print("n1 is greater")
+elif(n2>n3 and n2>n1):
+       print("n2 is greater")
+else:(n3>n1 and n3>n2)
+print("n3 is greater")
+'''
+# Leap year
+yr=int(input("enter year"))
+if(yr%100==0):
+    if(yr%400==0):
+        print("leap year")
+elif(yr%4==0):
+    print("leap Year")
+else:
+    print("NOT A LEAP YEAR")
+# CALCULATOR
+'''ch=input("ANY Operator")
+num1=int(input("1st no"))
+num2=int(input("2nd no"))
+if(ch=='+'):
+    print(num1+num2)
+elif(ch=='-'):
+    print(num1-num2)
+elif(ch=='*'):
+    print(num1*num2)
+elif(ch=='/'):
+    print(num1/num2)
+else:
+    print("Invalid")'''
+    
